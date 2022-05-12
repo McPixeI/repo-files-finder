89f00064e2fbce81971ca947e40341c1c0a0be9b
McPixeI <torres.88.bcn@gmail.com>
2022-05-13 00:19:13 +0200

    created Searcher with own state to avoid App rerenders

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import { useCallback, useState } from "react"
+import { useState } from "react"
 import { ExtensionsList } from "./components/ExtensionsList/ExtensionsList"
-import { TextInput } from "./components/UI/Forms/TextInput/TextInput"
-import { getURLdata } from "./utils/helpers"
+import { Searcher } from "./components/Searcher"
 
 function App () {
-  const [search, setSearch] = useState('')
+
   const [data, setData] = useState({})
 
-  const handleSubmit = useCallback((evt) => {
-    evt.preventDefault()
-    setData(getURLdata(search))
-  },[search])
-  
   return (
     <div className='App'>
       <h1 className='text-3xl font-bold mb-4'>
         Repo extensions finder
       </h1>
-      <form onSubmit={handleSubmit}>
-        <TextInput 
-          name='searcher'
-          value={search} 
-          onChange={evt => setSearch(evt.target.value)}
-        />
-      </form>
+      <Searcher setData={setData}/>
       <ExtensionsList data={data} />
     </div>
   )
---
+++ src/components/Searcher/Searcher.js
@@ -0,0 +1,23 @@
+import { useCallback, useState } from "react"
+import { getURLdata } from "../../utils/helpers"
+import {TextInput} from '../UI/Forms/TextInput'
+
+export const Searcher = ({setData}) => {
+
+    const [search, setSearch] = useState('')
+  
+    const handleSubmit = useCallback((evt) => {
+      evt.preventDefault()
+      setData(getURLdata(search))
+    },[search, setData])
+
+    return (
+        <form onSubmit={handleSubmit}>
+        <TextInput 
+          name='searcher'
+          value={search} 
+          onChange={evt => setSearch(evt.target.value)}
+        />
+      </form>
+    )
+}
