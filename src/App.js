aece04c3dbadb7b08bd8201142329dc9a2cbc093
McPixeI <torres.88.bcn@gmail.com>
2022-05-13 12:11:44 +0200

    print extensions count

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App () {
   const [data, setData] = useState({})
 
   return (
-    <div className='App'>
+    <div className='container mx-auto px-4 py-4'>
       <h1 className='text-3xl font-bold mb-4'>
         Repo extensions finder
       </h1>
--- src/components/ExtensionsList/ExtensionsList.js
+++ src/components/ExtensionsList/ExtensionsList.js
@@ -1,3 +1,4 @@
+import { useId } from 'react'
 import { getExtension } from '../../utils/helpers'
 import { useFiles } from '../../utils/hooks/use-files'
 import { Spinner } from '../UI/Spinner'
@@ -5,14 +6,12 @@ import { Spinner } from '../UI/Spinner'
 export const ExtensionsList = ({ data }) => {
   const { files, status, error } = useFiles(data)
 
-  const extensions = files.map(file => getExtension(file.path))
+  const paths = files.map(file => getExtension(file.path))
 
-  const extensionsCount = extensions.reduce((acc, val) => {
+  const extensions = paths.reduce((acc, val) => {
     acc[val] = (acc[val] || 0) + 1
     return acc
   }, {})
-
-  console.log(extensionsCount)
 
   if (status === 'loading') return <Spinner />
 
@@ -20,7 +19,12 @@ export const ExtensionsList = ({ data }) => {
 
   return (
     <>
-      {files.length ? files.map(file => <p key={file.sha}>{file.path}</p>) : null}
+
+      {Object.keys(extensions).length
+        ? (Object.keys(extensions).map(ext => {
+            return <p key={useId}><strong>{`${ext}: `}</strong>{extensions[ext]}</p>
+          }))
+        : null}
     </>
   )
 }
