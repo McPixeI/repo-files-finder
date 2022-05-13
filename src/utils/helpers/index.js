15a752f6da5b727d2c7f23026338514097f81d7f
McPixeI <torres.88.bcn@gmail.com>
2022-05-13 15:24:03 +0200

    clenup getURLdata helper

--- src/utils/helpers/index.js
+++ src/utils/helpers/index.js
@@ -1,18 +1,10 @@
 export function getURLdata (keyword) {
-  if (typeof keyword !== 'string' || !keyword.length) {
-    return null
-  }
   try {
     const url = new window.URL(keyword)
-    if (url.hostname === 'github.com') {
-      const pathArr = url.pathname.split('/').filter(Boolean)
-      return {
-        owner: pathArr[0],
-        repository: pathArr[1]
-      }
-    } else {
-      // To Do
-      return null
+    const pathArr = url.pathname.split('/').filter(Boolean)
+    return {
+      owner: pathArr[0],
+      repository: pathArr[1]
     }
   } catch (error) {
     console.log(error)
